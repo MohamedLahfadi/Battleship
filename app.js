5edc349850d98a74d1f1f56e71d6d24e1b9acef4
MohamedLahfadi <lahfadi.mohamed@yahoo.fr>
2017-06-28 19:19:06 +0200

    lastpart

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var LocalStrategy = require('passport-local').Strategy;
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
 connections = [];
-var io = require('socket.io').listen(app);
+var io = require('socket.io').listen(http);
 var http = require('http').Server(app);
 
 mongoose.connect('mongodb://localhost/battleship');
